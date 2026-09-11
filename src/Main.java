8145b64d4535b665059785f9fbe95073001f8b69
Marat Aruzhan <marataruzhan2007@gmail.com>
2026-09-11 21:33:57 +0500

    Initial domain model

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Assignment 1 - Builder Pattern");
+        System.out.println("Domain: Computer Configuration");
+
+    }
+}
